9636e0ca33bdd5a0970502011ccac59538d3f74b
Nataliia510 <natalyavarts@gmail.com>
2023-06-27 00:00:12 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,7 +2,20 @@
 
 
 $( document ).ready(function() {
-    $('.first-screen-slider').slick();
+    $('.first-screen-slider').slick({
+      dots: true,
+      speed: 1000,
+      appendArrows: ".container-arrows",
+      responsive: [{
+              breakpoint: 991,
+              settings: {
+                  arrows: false,
+                  fade: true,
+              }
+          },
+
+      ]
+  });
 
     Fancybox.bind('[data-fancybox="gallery"]', {
       });
